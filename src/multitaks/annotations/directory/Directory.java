42336b96a227b80748672cb429fb98e81a08362c
dogiloki <wolftri2002@gmail.com>
2023-04-02 02:27:11 -0600

    Hacer que se herede la anotación Directory

--- src/multitaks/annotations/directory/Directory.java
+++ src/multitaks/annotations/directory/Directory.java
@@ -2,6 +2,7 @@ package multitaks.annotations.directory;
 
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import multitaks.enums.DirectoryType;
@@ -11,6 +12,7 @@ import multitaks.enums.DirectoryType;
  * @author dogi_
  */
 
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Directory{
