c5be7fecd6034f99141e3069ef8ae1456127f29b
dogiloki <wolftri2002@gmail.com>
2023-04-02 02:19:22 -0600

    Remove importación de librería no útilizada en ModelDirectory

--- src/multitaks/directory/ModelDirectory.java
+++ src/multitaks/directory/ModelDirectory.java
@@ -1,7 +1,6 @@
 package multitaks.directory;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.reflect.TypeToken;
