9e740d4322f54adb1c6eb988e813b48ef1bde624
dogiloki <wolftri2002@gmail.com>
2023-04-20 23:45:06 -0600

    ObjectId se le puede específicar el inicio del conteo, sino se específica inicia en 0 cada vez que es llamado por el método static generate() o cada que es instanciado

--- src/database/ObjectId.java
+++ src/database/ObjectId.java
@@ -13,18 +13,30 @@ import multitaks.Function;
 
 public class ObjectId{
     
-    private static long count=0;
-    
-    public ObjectId(){
-        
-    }
+    private long count;
     
     public static String generate(){
+        return new ObjectId().get();
+    }
+    
+    public static String generate(long count){
+        return new ObjectId(count).get();
+    }
+    
+    public ObjectId(){
+        this.count=0;
+    }
+    
+    public ObjectId(long count){
+        this.count=count;
+    }
+    
+    public String get(){
         try{
             ByteArrayOutputStream out_array=new ByteArrayOutputStream();
-            out_array.write(ObjectId.getTimestamp());
-            out_array.write(ObjectId.getMac());
-            out_array.write(ObjectId.getCount());
+            out_array.write(this.getTimestamp());
+            out_array.write(this.getMac());
+            out_array.write(this.getCount());
             byte[] hash=MessageDigest.getInstance("SHA-256").digest(out_array.toByteArray());
             StringBuilder hex=new StringBuilder();
             for(int index=0; index<hash.length; index++){
@@ -37,7 +49,7 @@ public class ObjectId{
         return null;
     }
     
-    private static byte[] getTimestamp(){
+    private byte[] getTimestamp(){
         long timestamp=System.currentTimeMillis();
         byte[] timestamp_bytes={
             (byte)((timestamp>>24) & 0xFF),
@@ -48,14 +60,14 @@ public class ObjectId{
         return timestamp_bytes;
     }
     
-    private static byte[] getMac(){
+    private byte[] getMac(){
         byte[] mac=Function.getMac().getBytes();
         return Arrays.copyOfRange(mac,0,3);
     }
     
-    private static byte[] getCount(){
-        ObjectId.count++;
-        byte[] count=String.valueOf(ObjectId.count).getBytes();
+    private byte[] getCount(){
+        this.count++;
+        byte[] count=String.valueOf(this.count).getBytes();
         return Arrays.copyOfRange(count,0,3);
     }
     
